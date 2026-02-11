0a2e3507c610b22c313f522a51a4bf70f1eab450
Simon <114744067+SimonXi77@users.noreply.github.com>
2026-02-11 10:14:28 +0000

    feat: Add features, extensions, and postCreate script

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update
 sudo apt-get install s1
-echo "export PATH=$PATH:/usr/games" >> ~/.bashrc
-echo "export PATH=$PATH:/usr/games" >> ~/.zshrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
